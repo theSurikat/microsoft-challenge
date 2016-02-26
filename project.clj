7c861c452206e01b0439af831155cd3e67ae59d4
Grant Everett <Geverett94@gmail.com>
2016-02-26 13:38:47 -0600

    Did one and a half problems

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"][org.clojure/math.combinatorics "0.1.1"][org.clojure/data.csv "0.1.3"]]
   :main ^:skip-aot microsoft-challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/microsoft_challenge/core.clj
+++ src/microsoft_challenge/core.clj
@@ -1,10 +1,80 @@
 (ns microsoft-challenge.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:require [clojure.math.combinatorics :as combo])
+  (:require [clojure.data.csv :as csv]))
 
-(def big-string "")
+(def outPut "output.txt")
+(def big-string "Integer-set-product-combinations_InputForSubmission_2.txt")
+(def practice "PracticeInput.txt")
 
-(defn file-to-list [file]
+(defn multi-tenth [x]
+  (* x .1))
+
+(defn multi-quarter [x]
+  (* x .25))
+
+(defn multi-half [x]
+  (* x .5))
+
+(def our-table
+  {
+    :O multi-tenth
+    :L 100
+    :M 100
+    :F 100
+    :T 100
+    :S multi-half
+    :B multi-half
+    :W multi-half
+    :C multi-quarter
+    })
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(defn file-to-list-string [file]
+ "Takes a text file and turns it to a list of it's contents"
+ (map parse-int
+   (str/split (slurp file) #",")))
+
+(defn find-prods [file]
+    (drop 1
+      (sort
+        (map #(apply * %)
+          (map #(into [] %)
+            (combo/subsets (file-to-list file)))))))
+
+(defn writeDaStuff [fileIn fileOut]
+  (spit fileOut
+    (apply str
+      (interpose ","
+        (map str
+          (find-prods fileIn))))))
+
+
+(defn file-to-list-string [file]
   "Takes a text file and turns it to a list of it's contents"
-  (map #(str/replace % #"\"" "")
-    (str/split (slurp file) #",")))
+  (str/split (slurp file) #"\r\n"))
+
+(defn string-list-to-vectors [file]
+  "Turns a list of strings into vectors"
+  (map #(into [] %)
+    (map #(str/split % #"")
+      (drop 1
+          (file-to-list-string file)))))
+
+(defn set-value [set idx]
+  (let [cur (nth set idx)]
+    (if (not (= cur "F"))
+      (let [num-cottage (find-cottages set idx)])
+      (let [open-land (find-all-land set idx)]))
+    (cond
+      (= cur "L") (assoc set idx
+                    ["wood" (+ 100 (* 100 (+ (* num-cottage 0.25) (if (saw-mil?) 0.5 0))))])
+      (= cur "M") (assoc set idx
+                    ["ore" (+ 100 (* 100 (+ (* num-cottage 0.25) (if (black-smith?) 0.5 0))))])
+      (= cur "F") (assoc set idx
+                    ["food" (+ 100 (* 100 (+ (* open-land 0.1) (if (wind-mill?) 0.5 0))))])
+      (= cur "T") (assoc set idx
+                    ["gold" (+ 100 (* 100 (* num-cottage 0.25))) ]))))
